7cf3b72531ce526944ee6b339fceed9c17eb4e07
LeonardoVFF <leonardovff@gmail.com>
2016-12-28 22:09:24 -0300

    Adição de menu drop-down

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,33 @@
-var app = {
+var get = {
+    item: function(item, dom){
+        return typeof(dom)!="undefined"?dom.querySelector(item):document.querySelector(item);
+    },
+    all: function(itens, dom){
+        return typeof(dom)!="undefined"?dom.querySelectorAll(itens):document.querySelectorAll(itens);
+    }
+},
+app = {
     init: function(){
-        console.log("Entrou");
         app.setEvents();
     },
     setEvents: function(){
-        document.querySelectorAll('.tooltip').forEach(function(arr){
-            arr.addEventListener("hover",function(e){ 
-                console.log(e);
-                e.stopPropagation();
-                e.preventDefault();
-                return false;
-            }, false);
-        });
+        get.item("body").addEventListener('click', function(e) {
+            get.all(".navbar-drop-item.active").forEach(function(arr){
+                arr.className = "navbar-drop-item";
+            });
+            var target = e.target; 
+            if(target.className == "material-icons" || target.tagName == "SPAN"){
+                if(target.parentNode.className == "navbar-drop-open"){
+                    target = target.parentNode;
+                }
+            }
+            console.log(target);
+            if(target.className == "navbar-drop-open"){
+               if(target.className.indexOf("active")==-1){
+                    target.parentNode.className += " active";
+                } 
+            }
+        }); 
     }
 }
-window.onload = app.init();
-console.log('entrou');
-$('[title]').hover(
-   function(e) {
-       console.log("entrou");
-       e.preventDefault();
-   },
-   function() { }
-);
+app.init();
